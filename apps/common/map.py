44bc8eb907c11758d100b26fdf1beddcfbae8d7c
Andrian Kravchenko <andriankrav@gmail.com>
2023-12-01 12:31:59 +0200

    Add action map. Add restriction for agent actions. Add passability. Fix issues. (#2)
    * Update folder path
    
    * Fix issues
    
    * test and visualize 1-1 fight
    
    * Update plot to display only start and end situation
    
    * - change to absolute path
    - fix pygame issues for mac
    
    * Add passability for units
    
    * Add outcome display
    
    * Add leader move
    
    * Update passability
    
    ---------
    
    Co-authored-by: Andrian Kravchenko <akravchenko@gloriumtech.com>

--- apps/common/map.py
+++ apps/common/map.py
@@ -10,9 +10,8 @@ class Field:
     def get_vulnerability(self):
         return VULNERABILITY_MAPPER[self.vulnerability]
 
-
 class Map:
-    def __init__(self, terrain: np.array = None) -> None:
+    def __init__(self, frontline_longtitude, terrain: np.array = None) -> None:
         """Map object that is created using perlin noise"""
         self.max_longtitude, self.max_latitude = terrain.shape
         # self.terrain = self.__convert_to_np_map(terrain)
@@ -22,6 +21,8 @@ class Map:
             (0.4, 0.6): Cell.MOUNTAIN.value["value"],
         }
         self.terrain = self._generate_map_using_perlin_noise()
+        self.action_map = np.zeros(terrain.shape)
+        self.frontline_longtitude = frontline_longtitude
 
     def _generate_map_using_perlin_noise(self) -> np.array:
         """Generate map using perlin noise
@@ -35,7 +36,7 @@ class Map:
 
         for x in range(self.max_longtitude):
             for y in range(self.max_latitude):
-                noise_value = noise.snoise2(x / 10, y / 10, octaves=6)
+                noise_value = noise.snoise2(x / 10, y / 20, octaves=6)
                 terrain[x, y] = self._map_noise_to_cell(noise_value)
 
         return terrain
@@ -48,6 +49,22 @@ class Map:
                 return cell_value
 
         return Cell.EMPTY.value["value"]
+    
+    def update_action_map(self,latitude, longitutte, value):
+        if (self._is_in_range(latitude,longitutte)):
+            self.action_map[latitude][longitutte] = value
+    
+    def can_move_to_point(self,latitude, longitutte, passability):
+        return self._is_point_available(latitude,longitutte) and (self.terrain[longitutte][latitude] <= passability)
+
+    def _is_point_available(self,latitude, longitutte):
+        return self.action_map[latitude][longitutte] == 0
+    
+    def clear_unit(self,latitude, longitutte):
+        self.action_map[latitude][longitutte] = 0
+
+    def _is_in_range(self,latitude, longitutte):
+        return (latitude < (self.max_latitude)) & (longitutte < (self.max_longtitude))
 
 
 VULNERABILITY_MAPPER = {1: 0.8, 2: 0.7, 3: 0.2}
