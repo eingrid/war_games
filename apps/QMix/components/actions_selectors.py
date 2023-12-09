a7d721b209c4b1dd5e9b639f8b360b9b1c0c1e99
leshchenkoM <leshchenkomaksym@gmail.com>
2023-12-09 14:14:30 +0200

    added qmix development part

---
+++ apps/QMix/components/actions_selectors.py
@@ -0,0 +1,57 @@
+import torch as th
+import numpy as np
+
+class EpsilonGreedyActionSelector():
+
+    def __init__(self, args):
+        self.args = args
+        # self.epsilon = epsilon
+
+        self.schedule = DecayThenFlatSchedule(args.epsilon_start, args.epsilon_finish, args.epsilon_anneal_time,
+                                              decay="linear")
+        self.epsilon = self.schedule.eval(0)
+
+    def select_action(self, agent_inputs, avail_actions, t_env, test_mode=False):
+
+        # Assuming agent_inputs is a batch of Q-Values for each agent bav
+        self.epsilon = self.schedule.eval(t_env)
+
+        if test_mode:
+            # Greedy action selection only
+            self.epsilon = self.args.evaluation_epsilon
+
+        # mask actions that are excluded from selection
+        masked_q_values = agent_inputs.clone()
+        masked_q_values[avail_actions == 0.0] = -float("inf")  # should never be selected!
+
+        random_numbers = th.rand_like(agent_inputs[:, :, 0])
+        pick_random = (random_numbers < self.epsilon).long()
+        random_actions = Categorical(avail_actions.float()).sample().long()
+
+        picked_actions = pick_random * random_actions + (1 - pick_random) * masked_q_values.max(dim=2)[1]
+        return picked_actions
+
+
+class DecayThenFlatSchedule():
+
+    def __init__(self,
+                 start,
+                 finish,
+                 time_length,
+                 decay="exp"):
+
+        self.start = start
+        self.finish = finish
+        self.time_length = time_length
+        self.delta = (self.start - self.finish) / self.time_length
+        self.decay = decay
+
+        if self.decay in ["exp"]:
+            self.exp_scaling = (-1) * self.time_length / np.log(self.finish) if self.finish > 0 else 1
+
+    def eval(self, T):
+        if self.decay in ["linear"]:
+            return max(self.finish, self.start - self.delta * T)
+        elif self.decay in ["exp"]:
+            return min(self.start, max(self.finish, np.exp(- T / self.exp_scaling)))
+    pass
